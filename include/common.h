d429e06741be739d09d59e707c15cfffac20fd49
Javier C <66850752+franckey02@users.noreply.github.com>
2026-01-03 21:45:56 -0400

    Update common.h

--- include/common.h
+++ include/common.h
@@ -1,76 +1,49 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include <string>
-#include <vector>
-#include <map>
-
-// Tipos de distribuciones soportadas
-enum class Distro {
-    UNKNOWN,
-    ARCH,
-    DEBIAN,
-    UBUNTU,
-    FEDORA,
-    OPENSUSE,
-    VOID,
-    NIXOS,
-    RHEL,
-    CENTOS,
-    MANJARO,
-    POPOS,
-    LINUXMINT
-};
-
-// Tipos de gestores de paquetes
-enum class PackageManager {
-    UNKNOWN,
-    PACMAN,
-    APT,
-    DNF,
-    ZYPPER,
-    XBPS,
-    NIX,
-    YUM
-};
-
-// Estructura para información del sistema
-struct SystemInfo {
-    Distro distribution;
-    std::string version;
-    PackageManager package_manager;
-    std::string architecture;
-    std::string session_type;
-    bool is_root;
-    
-    SystemInfo() : distribution(Distro::UNKNOWN), 
-                   package_manager(PackageManager::UNKNOWN),
-                   is_root(false) {}
-};
-
-// Constantes
-const std::map<Distro, std::string> DISTRO_NAMES = {
-    {Distro::ARCH, "Arch Linux"},
-    {Distro::DEBIAN, "Debian"},
-    {Distro::UBUNTU, "Ubuntu"},
-    {Distro::FEDORA, "Fedora"},
-    {Distro::OPENSUSE, "openSUSE"},
-    {Distro::VOID, "Void Linux"},
-    {Distro::NIXOS, "NixOS"},
-    {Distro::RHEL, "RHEL"},
-    {Distro::CENTOS, "CentOS"},
-    {Distro::MANJARO, "Manjaro"},
-    {Distro::POPOS, "Pop!_OS"},
-    {Distro::LINUXMINT, "Linux Mint"}
-};
-
-// Funciones auxiliares
+// Agregar estas funciones al namespace utils en common.h
 namespace utils {
-    std::string exec_command(const std::string& cmd);
-    bool file_exists(const std::string& path);
-    std::string trim(const std::string& str);
-    std::vector<std::string> split(const std::string& str, char delimiter);
-    bool contains(const std::string& str, const std::string& substr);
+    std::string exec_command(const std::string& cmd) {
+        char buffer[128];
+        std::string result = "";
+        FILE* pipe = popen(cmd.c_str(), "r");
+        if (!pipe) return "";
+        
+        try {
+            while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+                result += buffer;
+            }
+        } catch (...) {
+            pclose(pipe);
+            return "";
+        }
+        
+        pclose(pipe);
+        return result;
+    }
+    
+    bool file_exists(const std::string& path) {
+        std::ifstream f(path.c_str());
+        return f.good();
+    }
+    
+    std::string trim(const std::string& str) {
+        size_t first = str.find_first_not_of(" \t\n\r");
+        if (first == std::string::npos) return "";
+        size_t last = str.find_last_not_of(" \t\n\r");
+        return str.substr(first, (last - first + 1));
+    }
+    
+    std::vector<std::string> split(const std::string& str, char delimiter) {
+        std::vector<std::string> tokens;
+        std::stringstream ss(str);
+        std::string token;
+        
+        while (std::getline(ss, token, delimiter)) {
+            tokens.push_back(token);
+        }
+        
+        return tokens;
+    }
+    
+    bool contains(const std::string& str, const std::string& substr) {
+        return str.find(substr) != std::string::npos;
+    }
 }
-
-#endif // COMMON_H
